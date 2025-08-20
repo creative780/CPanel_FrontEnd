2568b6252e9dbe871da48ec5742b3fc92cf7c0e4
Abdullah <abdullah.work027@gmail.com>
2025-08-20 10:55:24 +0500

    last commit

--- next.config.ts
+++ next.config.ts
@@ -2,39 +2,25 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'scontent.fisb1-2.fna.fbcdn.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/uploads/**',
-      },
+      // Django media served from your API domain
+      { protocol: 'https', hostname: 'api.click2print.store', pathname: '/media/**' },
+
+      // CDNs you already use
+      { protocol: 'https', hostname: 'storage.googleapis.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'scontent.fisb1-2.fna.fbcdn.net', pathname: '/**' },
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'i.pravatar.cc', pathname: '/**' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com', pathname: '/**' },
+
+      // Local dev (Django runserver)
+      { protocol: 'http', hostname: '127.0.0.1', port: '8000', pathname: '/media/**' },
+      { protocol: 'http', hostname: 'localhost',  port: '8000', pathname: '/media/**' },
+
+      // Local dev if you test via Gunicorn on 8001
+      { protocol: 'http', hostname: '127.0.0.1', port: '8001', pathname: '/media/**' },
+      { protocol: 'http', hostname: 'localhost',  port: '8001', pathname: '/media/**' },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
